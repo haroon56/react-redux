addf714ed1d95223669741c3647d7fef28b7ea7e
haroon56 <haroon.khalil2100@gmail.com>
2018-02-11 00:25:37 +0500

    seconda ttempt

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4gQe_PoGFj512XieVlSicD4YlLl4mC-M",
+    authDomain: "redux-firebase-753b8.firebaseapp.com",
+    databaseURL: "https://redux-firebase-753b8.firebaseio.com",
+    projectId: "redux-firebase-753b8",
+    storageBucket: "redux-firebase-753b8.appspot.com",
+    messagingSenderId: "619870146064"
+};
